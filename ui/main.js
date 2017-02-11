2ef2e5784830c6fbb3a159ab62a8366473130bf0
rishi1596 <rishabh.jain53@gmail.com>
2017-02-12 01:13:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,32 @@
-var usercomment = document.getElementById('comment');
-var comment = usercomment.value;
+
 var submit = document.getElementById('submitcomment');
 
 submit.onclick = function()
 {
+    var request = new XMLHttpRequest();
     
+    request.onreadystateChange = function()
+    {
+        if(request.readyState == XMLHttpRequest.DONE)
+        {
+            if(request.status == 200)
+            {
+                var comments = request.responseText;
+                comments = JSON.parse(comments);
+                var allcomments = '';
+                for(var i = 0; i<comments.length ; i++)
+                {
+                    allcomments = '<li>' + comments[i] + '</li>';
+                }
+                var list = document.getElementById('commentlist');
+                list.innerHTML = allcomments;
+            }
+        }
+    }
+    
+var usercomment = document.getElementById('comment');
+var comment = usercomment.value;
+
+reuest.open('GET','http://rishi1596.imad.hasura-app.io/article-comment?comment=' + comment,true);
+request.send(null);
 };
